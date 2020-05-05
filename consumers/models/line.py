38d6b1178602ae1aee5bef4cacf5cffea6a543f7
Kelvin Jimenez <kelvin@kelvin.cr>
2020-05-05 18:26:12 +0700

    implementation of Line consumer

--- consumers/models/line.py
+++ consumers/models/line.py
@@ -32,6 +32,7 @@ class Line:
     def _handle_arrival(self, message):
         """Updates train locations"""
         value = message.value()
+        logger.debug(value)
         prev_station_id = value.get("prev_station_id")
         prev_dir = value.get("prev_direction")
         if prev_dir is not None and prev_station_id is not None:
@@ -56,16 +57,16 @@ class Line:
 
     def process_message(self, message):
         """Given a kafka message, extract data"""
-        # TODO: Based on the message topic, call the appropriate handler.
-        if True: # Set the conditional correctly to the stations Faust Table
+        topic_name = message.topic()
+        if topic_name == "org.chicago.cta.stations.table.v1": # Set the conditional correctly to the stations Faust Table
             try:
                 value = json.loads(message.value())
                 self._handle_station(value)
             except Exception as e:
                 logger.fatal("bad station? %s, %s", value, e)
-        elif True: # Set the conditional to the arrival topic
+        elif topic_name == "org.chicago.cta.station.arrivals": # Set the conditional to the arrival topic
             self._handle_arrival(message)
-        elif True: # Set the conditional to the KSQL Turnstile Summary Topic
+        elif topic_name == "TURNSTILE_SUMMARY": # Set the conditional to the KSQL Turnstile Summary Topic
             json_data = json.loads(message.value())
             station_id = json_data.get("STATION_ID")
             station = self.stations.get(station_id)
